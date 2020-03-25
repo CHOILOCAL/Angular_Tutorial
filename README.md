5fb4793f3cd3dfc5b72f16d6006eb1a585705ff1
CHOILOCAL <choihyunji1103@gmail.com>
2020-03-25 13:49:47 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ywuvhi-xitsin
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ywuvhi-xitsin)
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [
+    // `providedIn`을 사용했기 때문에 프로바이더는 등록하지 않습니다.
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
